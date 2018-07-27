06670938e52a1409308dd3d7ed084003ae690676
ThibDT <thibault.de.tourdonnet@gmail.com>
2018-07-27 13:26:28 +0200

    initial commit

---
+++ js/ex2.js
@@ -0,0 +1,32 @@
+/* 
+En l'état actuel cette fonction ne sert strictement à rien.
+Ajoutez lui une valeur de retour (Hello, World!)
+*/
+function ex1() {
+    "Hello, World!"
+}
+
+// Pour celle-ci débrouillez vous !
+function ex2() {
+    return a * b;
+}
+
+
+/*
+Cette fonction doit retourner un tableau de nombre impaires.
+Elle comporte (entre autre) des erreurs de syntaxe empêchant
+l'exécution de tout le fichier.
+Décommentez la une fois les autres exercices validés.
+*/
+
+/*
+function ex3(a) {
+    var oddNumbers = [];
+    for(i <= a) {
+        if i%2 {
+            oddNumbers.push(i)
+        }
+    }
+    return;
+}
+*/
